1efdbe7eef33772dfb69668aaa855fe0b3a5b384
Loc Pham <loc.pt2312@gmail.com>
2020-07-07 23:17:32 -0700

    update login screen use 2 modal

--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -1,11 +1,21 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 import React from 'react';
 
 const HomeScreen = () => (
-  <SafeAreaView>
-    <Text>Screen: Home</Text>
+  <SafeAreaView style={styles.container}>
+    <Text style={styles.title}>Screen: Home</Text>
   </SafeAreaView>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+  },
+});
 
 export default HomeScreen;
--- src/scenes/login/index.js
+++ src/scenes/login/index.js
@@ -1,15 +1,46 @@
-import {SafeAreaView, Text, TouchableHighlight} from 'react-native';
+import {ModalLogin, ModalRegister} from 'organisms';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 import React from 'react';
 
-const LoginScreen = ({navigation}) => (
-  <SafeAreaView>
-    <Text>Screen: Login</Text>
+class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: '',
+      password: '',
+      isModalLogin: true,
+    };
+    this.onToggle = this.onToggle.bind(this);
+  }
 
-    <TouchableHighlight onPress={() => navigation.navigate('Home')}>
-      <Text>Go to home</Text>
-    </TouchableHighlight>
-  </SafeAreaView>
-);
+  onToggle() {
+    this.setState({isModalLogin: !this.state.isModalLogin});
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ModalLogin
+          IsVisible={this.state.isModalLogin}
+          changeModal={this.onToggle}
+        />
+
+        <ModalRegister
+          IsVisible={!this.state.isModalLogin}
+          changeModal={this.onToggle}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'aqua',
+  },
+});
 
 export default LoginScreen;
