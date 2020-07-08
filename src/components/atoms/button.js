985adb8822a3de459a88f0bf466d9dd8dabe565f
Loc Pham <loc.pt2312@gmail.com>
2020-07-07 23:37:04 -0700

    atom button can override style

--- src/components/atoms/button.js
+++ src/components/atoms/button.js
@@ -2,23 +2,27 @@ import {StyleSheet, Text, TouchableHighlight} from 'react-native';
 
 import React from 'react';
 
-const Button = ({title, action}) => (
-  <TouchableHighlight style={styles.button} onPress={action}>
-    <Text style={styles.text}>{title}</Text>
+const Button = ({title, action, buttonStyle, textStyle}) => (
+  <TouchableHighlight
+    style={{...buttonStyle, ...styles.button}}
+    onPress={action}>
+    <Text style={{...textStyle, ...styles.text}}>{title}</Text>
   </TouchableHighlight>
 );
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: 'aqua',
+    backgroundColor: 'white',
+    borderColor: '#19CC95',
+    borderStyle: 'solid',
+    borderWidth: 1,
     borderRadius: 20,
     paddingVertical: 10,
     paddingHorizontal: 20,
     elevation: 2,
   },
   text: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: '#19CC95',
     textAlign: 'center',
   },
 });
